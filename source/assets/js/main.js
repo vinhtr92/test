879dc63c6c95e7df6ca730d6a9be79159be1ca18
vinhcoder929 <tranvinh@hotmail.com.vn>
2017-07-09 05:21:08 +0700

    hoan thien v1.3

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -444,3 +444,40 @@
 
 
 })(jQuery);
+
+
+
+
+
+// COUNTDOWN TIMER
+var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+var countUpTimer = new Date("Jan 5, 2016 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+  var distanceUp = now - countUpTimer ;
+
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var daysUp = Math.floor(distanceUp / (1000 * 60 * 60 * 24));
+
+  // Display the result in the element with id="demo"
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+
+   document.getElementById("day").innerHTML = days;
+   document.getElementById("hour").innerHTML = hours;
+   document.getElementById("minute").innerHTML = minutes;
+   document.getElementById("second").innerHTML = seconds;
+   document.getElementById("count-up").innerHTML = daysUp;
+}, 1000);
